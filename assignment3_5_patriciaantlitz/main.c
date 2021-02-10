ec44b6a796b52081a7a51c9671dce2b233c42bf4
patybn3 <paty.nigro@gmail.com>
2021-02-10 02:32:13 -0500

    Initial Commit

---
+++ assignment3_5_patriciaantlitz/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  assignment3_5_patriciaantlitz
+//
+//  Created by Patricia Antlitz on 2/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
